2f75bef87ea9a5049dde7d69bd3f91784473b25c
Brandon James <brandonj2858@gmail.com>
2020-06-08 18:58:59 -0400

    mass fetch done

--- server.js
+++ server.js
@@ -16,6 +16,79 @@ db.once('open', () => console.log('Connected to database'))
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 
+
+let schoolsList = [  'Air Force',         'Akron',                 'Alabama',
+  'Appalachian State', 'Arizona',               'Arizona State',
+  'Arkansas',          'Arkansas State',        'Army',
+  'Auburn',            'Ball State',            'Baylor',
+  'Boise State',       'Boston College',        'Bowling Green',
+  'Buffalo',           'BYU',                   'California',
+  'Central Michigan',  'Charlotte',             'Cincinnati',
+  'Clemson',           'Coastal Carolina',      'Colorado',
+  'Colorado State',    'Connecticut',           'Duke',
+  'East Carolina',     'Eastern Michigan',      'Florida',
+  'Florida Atlantic',  'Florida International', 'Florida State',
+  'Fresno State',      'Georgia',               'Georgia Southern',
+  'Georgia State',     'Georgia Tech',          "Hawai'i",
+  'Houston',           'Illinois',              'Indiana',
+  'Iowa',              'Iowa State',            'Kansas',
+  'Kansas State',      'Kent State',            'Kentucky',
+  'Liberty',           'Louisiana',             'Louisiana Monroe',
+  'Louisiana Tech',    'Louisville',            'LSU',
+  'Marshall',          'Maryland',              'Memphis',
+  'Miami',             'Miami (OH)',            'Michigan',
+  'Michigan State',    'Middle Tennessee',      'Minnesota',
+  'Mississippi State', 'Missouri',              'Navy',
+  'NC State',          'Nebraska',              'Nevada',
+  'New Mexico',        'New Mexico State',      'North Carolina',
+  'Northern Illinois', 'North Texas',           'Northwestern',
+  'Notre Dame',        'Ohio',                  'Ohio State',
+  'Oklahoma',          'Oklahoma State',        'Old Dominion',
+  'Ole Miss',          'Oregon',                'Oregon State',
+  'Penn State',        'Pittsburgh',            'Purdue',
+  'Rice',              'Rutgers',               'San Diego State',
+  'San José State',    'SMU',                   'South Alabama',
+  'South Carolina',    'Southern Mississippi',  'South Florida',
+  'Stanford',          'Syracuse',              'TCU',
+  'Temple',            'Tennessee',             'Texas',
+  'Texas A&M',         'Texas State',           'Texas Tech',
+  'Toledo',            'Troy',                  'Tulane',
+  'Tulsa',             'UAB',                   'UCF',
+  'UCLA',              'UMass',                 'UNLV',
+  'USC',               'Utah',                  'Utah State',
+  'UTEP',              'UT San Antonio',        'Vanderbilt',
+  'Virginia',          'Virginia Tech',         'Wake Forest',
+  'Washington',        'Washington State',      'Western Kentucky',
+  'Western Michigan',  'West Virginia',         'Wisconsin',
+  'Wyoming'
+]
+
+let schoolsTest = ['Appalachian State', 'Arizona']
+
+
+/*
+function rosterFetch() {
+  schoolsList.forEach((school) => {
+    fetch(`https://api.collegefootballdata.com/roster?team=${school}&year=2019`)
+      .then(res => res.json())
+      .then(resObj => {
+        resObj.forEach((player) => player.school = school)
+        return resObj
+       })
+      .then(resObj => db.collection('players').insertMany(resObj))
+  })
+}
+
+rosterFetch()
+
+*/
+
+
+
+
+
+
+
 /*
 fetch('https://api.collegefootballdata.com/roster?team=Washington%20State&year=2019')
   .then(res => res.json())
